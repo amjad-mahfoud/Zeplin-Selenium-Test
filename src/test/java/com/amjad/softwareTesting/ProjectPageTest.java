20b5ba3776cd6a9be4124b7bde1f07c85086baf2
Amjad Mahfoud <amjadoof@gmail.com>
2022-05-08 17:46:51 +0200

    allow multiple file download

--- src/test/java/com/amjad/softwareTesting/ProjectPageTest.java
+++ src/test/java/com/amjad/softwareTesting/ProjectPageTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,44 +16,52 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProjectPageTest {
     public WebDriver driver;
-    ConfigFileReader configFileReader;
+
+    private boolean downloaded = false;
 
     @BeforeAll
     public void setup() {
-        ChromeOptions options = new ChromeOptions();
+        var options = new ChromeOptions();
         Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("download.default_directory", "src//downloads"); // Bypass default download directory in Chrome
+        prefs.put("safebrowsing.enabled", "false"); // Bypass warning message, keep file anyway (for .exe, .jar, etc.)
         prefs.put("profile.default_content_setting_values.automatic_downloads", 1);
-//        prefs.put("download.default_directory", "src//downloads");
         options.setExperimentalOption("prefs", prefs);
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
     @Test
     public void downloadMultipleFiles() throws InterruptedException {
-        MainPage mainPage = new MainPage(this.driver);
+        var mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.getLoginPage();
         DashboardPage dashboardPage = loginPage.clickLogin();
         ProjectPage projectPage = dashboardPage.getFirstProject();
         System.out.println(projectPage.getBodyText());
         List<WebElement> btns = projectPage.getAllButtons();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         for (WebElement btn : btns) {
             btn.click();
             projectPage.clickDownloadButton();
         }
+
+//        JavascriptExecutor jse = (JavascriptExecutor) driver;
+//        System.out.println(jse.executeScript("browserstack_executor: {\"action\": \"fileExists\", \"arguments\": {\"fileName\": \"9.zip\"}}"));
     }
 
-//    @AfterAll
-//    public void close() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
+    @AfterAll
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
